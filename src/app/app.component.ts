bd215d05bf6e239d7b1187f559a891ecc3794edb
HiagoCarlos <hiagocarlos77112233@gmail.com>
2025-09-02 21:29:41 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { RodapeComponent } from "./components/rodape/rodape.component";
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ProdutosComponent } from './components/produtos/produtos.component';
+import { NoticiaComponent } from './components/noticia/noticia.component';
+
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, MenuComponent, RodapeComponent, GaleriaComponent, ProdutosComponent, NoticiaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
